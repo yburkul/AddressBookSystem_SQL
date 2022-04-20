d19d683549091219978a0106d57ae90b4f321606
yburkul <yburkul123@gmail.com>
2022-04-20 22:21:35 +0530

    Added:Create a Address Book Table

--- AddressBookService.sql
+++ AddressBookService.sql
@@ -1,2 +1,15 @@
 create database AddressBook_Service;
 use AddressBook_Service;
+
+create table AddressBook(
+ID int identity(1,1) primary key,
+First_Name varchar(50),
+Last_Name varchar(50),
+Address varchar(200),
+city varchar(50),
+state varchar(50),
+Zip bigint,
+PhoneNumber bigint,
+Email varchar(50));
+
+select * from AddressBook;
