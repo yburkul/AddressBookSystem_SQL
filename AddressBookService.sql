23825b55764f965675cc7338d52b5d6de9e13f15
yburkul <yburkul123@gmail.com>
2022-04-21 14:54:57 +0530

    Added:Add Person To both Friend and Family

--- AddressBookService.sql
+++ AddressBookService.sql
@@ -32,9 +32,23 @@ select * from AddressBook where City = 'Jalna' order by First_Name ASC;
 Alter table AddressBook
 Add Name varchar(100), Type varchar(100)
 
-update AddressBook set Type = 'FriendsBook', Name = 'Friend' where First_Name in ('Yogesh','Atish','Nikita','Arjun');
-update AddressBook set Type = 'FamilyBook', Name = 'Family' where First_Name in ('Yogesh', 'Arjun');
+update AddressBook set Type = 'Friend', Name = 'FriendBook' where First_Name in ('Atish','Nikita');
+update AddressBook set Type = 'Family', Name = 'FamilyBook' where First_Name in ('Yogesh', 'Arjun');
 
 select * from AddressBook;
 
-select count(First_Name) as FamilyContact from AddressBook where Type = 'FamilyBook';
+select count(First_Name) as FamilyContact from AddressBook where Type = 'Family';
+
+insert into AddressBook(First_Name,Last_Name,Address,City,State,Zip,PhoneNumber,Email)values
+('Ganesh','Bhagile','Chikhali','Deulgaon','Maharshtra',4400112,9911005522,'ganesh@gmail.com');
+
+update AddressBook set Type = 'Friend', Name = 'FriendBook' where First_Name = 'Ganesh';
+
+insert into AddressBook(First_Name,Last_Name,Address,City,State,Zip,PhoneNumber,Email)values
+('Amol','Mhaske','Giroli','Deulgaon','Maharshtra',4400103,7711002522,'amol123@gmail.com');
+update AddressBook set Type = 'Family', Name = 'FamilyBook' where First_Name = 'Amol';
+
+select * from AddressBook;
+insert into AddressBook(First_Name,Last_Name,Address,City,State,Zip,PhoneNumber,Email,Name,Type)values
+('Bharat','Mante','Shirpur','Buldana','Maharshtra',440303,8011902502,'bharat3@gmail.com','FriendBook','Friend'),
+('Sharad','Surushe','Aland','Aurangabad','Maharshtra',440303,8012302502,'sharad93@gmail.com','FamilyBook','Family');
